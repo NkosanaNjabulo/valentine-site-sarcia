25044ad332c7e8b628a261a7832cbd853b394e15
NkosanaNjabulo <148999633+NkosanaNjabulo@users.noreply.github.com>
2026-02-14 18:16:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const envelope = document.getElementById("envelope");
+const card = document.getElementById("card");
+const cardInner = document.getElementById("cardInner");
+const music = document.getElementById("bgMusic");
+
+let opened = false;
+
+envelope.addEventListener("click", () => {
+    if (!opened) {
+        envelope.classList.add("open");
+        card.classList.remove("hidden");
+        card.classList.add("show");
+        music.play();
+        opened = true;
+    }
+});
+
+cardInner.addEventListener("click", () => {
+    cardInner.classList.toggle("flipped");
+});
